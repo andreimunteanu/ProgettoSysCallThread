3341e948d638c5d125df25c12065a6334c1ca62c
niccolomarastoni <eightnine@gmail.com>
2013-06-24 16:25:16 +0300

    Update mem_init.c

--- mem_init.c
+++ mem_init.c
@@ -4,25 +4,18 @@ int read_integer(int fd){
   char c[1], buf[64];
   char *temp = buf;
   
-  if(read(fd,c,1) == -1 || *c == '\n'){
-    perror("read");
-    printf("codifica1\n");
-    exit(1);
-  }
+  if(read(fd,c,1) == -1 || *c == '\n') 
+    syserr("read(conf_file) failed.\n");
   
   while(*c != '\n' && *c != ' '){
     
     if((*c >= '0' && *c <= '9') )
       *(temp++) = *c;
-    else{
-      printf("codifica2\n");
-      exit(1);
-    }
+    else
+      syserr("Configuration file contains bad data.\n");
     
-    if(read(fd,c,1) == -1){
-      perror("read");
-      exit(1);
-    }
+    if(read(fd,c,1) == -1)
+      syserr("read(conf_file) failed.\n");
   }
   *temp = '\0';
   return atoi(buf);
@@ -36,24 +29,22 @@ void read_remaining_line(int fd, operation *cursor){
   char c[1], buf[64], operator;
   char *aux = buf;
 	
-  if(read(fd,c,1) == -1 || !is_operator(*c)){
-    printf("bitch\n");
-    exit(1);
-  }
+  if(read(fd,c,1) == -1 || !is_operator(*c))
+    syserr("Configuration file contains bad data.\n");
   operator = *c;
 	
-  if(read(fd,c,1) == -1 ) // da mettere a novanta
-    exit(1);
+  if(read(fd,c,1) == -1 ) 
+    syserr("read(conf_file) failed.\n");
 
   if(read(fd,c,1) == -1 || *c == '\n')
-    exit(1);
+    syserr("Configuration file contains bad data.\n");
 
   while(*c != '\n' && *c != EOF){
-		
     if((*c >= '0' && *c <= '9') )
       *(aux++) = *c;
+
     else
-      exit(1);
+     syserr("Configuration file contains bad data.\n");
 
     if(read(fd,c,1) == -1)
       exit(1);
@@ -68,6 +59,7 @@ void write_line(int fd, operation *cursor){
   read_remaining_line(fd, cursor);	
 }
 
+//obsolete??
 void copy_operations(int fd, int *proc_id, operation **operations,int lines){
   int i = 0;
   char buf[64];
@@ -76,7 +68,8 @@ void copy_operations(int fd, int *proc_id, operation **operations,int lines){
   while(lines--){
     proc_id[i++] = read_integer(fd);
     write_line(fd, cursor);		
-    printf("\t%d %c %d\n",cursor->num1, cursor->op,cursor->num2);
+    sprintf(buf,"\t%d %c %d\n",cursor->num1, cursor->op,cursor->num2);
+    //    print_to_video(buf);
     cursor++;
   }
 
